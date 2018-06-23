b3bfeb16c712d4f6ac8399e7c000a011ce71ec7e
MateuszOpole <mkazior09@gmail.com>
2018-06-23 18:25:22 +0200

    Kryptomaniak, 6 commit

--- kryptomaniak/settings.py
+++ kryptomaniak/settings.py
@@ -18,7 +18,7 @@ from django.conf import settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
