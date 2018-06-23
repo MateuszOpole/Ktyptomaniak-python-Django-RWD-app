19efab1d5e3fb8c4099c12834f4ca092ff2118d0
MateuszOpole <mkazior09@gmail.com>
2018-06-23 18:49:33 +0200

    Kryptomaniak, four commit

--- kryptomaniak/settings.py
+++ kryptomaniak/settings.py
@@ -18,6 +18,8 @@ from django.conf import settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
