fb220f966b8a35f7959ef24f390fdc2d3e7c8c6a
MateuszOpole <mkazior09@gmail.com>
2018-06-24 23:38:19 +0200

    added

--- kryptomaniak/settings.py
+++ kryptomaniak/settings.py
@@ -23,7 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-SSTATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
