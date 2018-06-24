06812378bedc1e067a0d1b47f0b8d30c1ca9b2ba
MateuszOpole <mkazior09@gmail.com>
2018-06-24 03:45:19 +0200

    added

--- kryptomaniak/settings.py
+++ kryptomaniak/settings.py
@@ -27,8 +27,10 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-
-
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '3uku#*7h8r4(hqaya+^j9!u61j_#l2wqj%1fk4ulagyuz!sf@k'
 
