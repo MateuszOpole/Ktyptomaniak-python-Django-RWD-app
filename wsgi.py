19a16eea4b129044fc2061e9973584b4f7d7aba6
MateuszOpole <mkazior09@gmail.com>
2018-06-24 03:55:52 +0200

    added

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,9 @@
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kryptomaniak.settings")
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-application = Cling(get_wsgi_application())
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kryptomaniak.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
